Fixed rotation issues (I think)

wtf/processing.py
@@ -103,10 +103,10 @@ def warp_img(raw_img_path, base, top, warped_img_size=WARPED_IMG_SIZE):
     embedded_base = base + offset
     embedded_top = top + offset
 
-    warped_base = ((0.8, 0.5) * warped_img_size).astype(np.int)
-    warped_top = ((0.1, 0.5) * warped_img_size).astype(np.int)
-    logger.debug("About to warp from {base, top = %r %r} to "
-                 "{warped_base, warped_top = %r %r}",
+    warped_base = ((0.9, 0.5) * warped_img_size).astype(np.int)
+    warped_top = ((0.05, 0.5) * warped_img_size).astype(np.int)
+    logger.debug("About to warp from {base, top = %r, %r} to "
+                 "{warped_base, warped_top = %r, %r}",
                  base, top, warped_base, warped_top)
 
     orig_vector = embedded_top - embedded_base